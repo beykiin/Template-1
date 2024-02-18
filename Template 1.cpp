248beaca3484e3fa8401507a6ba6f98878d46eac
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:29:10 +0300

    Add files via upload

---
+++ Template 1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+
+
+//template<typename T>
+//T max(T a,T b) {
+//    return (a > b) ? a : b;
+//
+//}
+//
+//
+//
+//int main() {
+//
+//    std::cout << max(10.1,5.5);
+//    std::cout << max(5, 10);
+//
+//}
+
